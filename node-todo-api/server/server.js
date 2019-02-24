5da12bbb2a2028f94e1c2624c7e582e3129bed6a
César Cruz <cesar.cruz182@gmail.com>
2019-02-24 11:18:03 -0600

    /todo/:id end point configured and tested

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { ObjectID } = require("mongodb");
 
 const { mongoose } = require("./db/mongoose");
 const { Todo } = require("./db/models/todo");
@@ -12,19 +13,41 @@ app.use(bodyParser.json());
 app.post("/todo", (req, res) => {
   const newTodo = new Todo({ text: req.body.text });
 
-  newTodo.save().then(doc => {
-    res.send(doc);
-  }).catch(error => {
-    console.log("here");
-    res.status(400).send(error);
-  });
+  newTodo
+    .save()
+    .then(doc => {
+      res.send(doc);
+    })
+    .catch(error => {
+      console.log("here");
+      res.status(400).send(error);
+    });
 });
 
 app.get("/todos", (req, res) => {
-  Todo.find({}).then(todos => {
-    res.send({todos})
-  });
-})
+  Todo.find({})
+    .then(todos => {
+      res.send({ todos });
+    })
+    .catch(error => {
+      res.status(401).send(error);
+    });
+});
+
+app.get("/todos/:id", (req, res) => {
+  const { id } = req.params;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(401).send({error: "Bad request"});
+  } else {
+    Todo.findOne({_id: id}).then(todo => {
+      if (todo)
+        return res.status(200).send(todo);
+        else
+        return res.status(404).send({error: "Item not found"});
+    });
+  }
+});
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
@@ -32,4 +55,4 @@ app.listen(3000, () => {
 
 module.exports = {
   app
-}
+};
--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -26,3 +26,56 @@ describe("POST /todo", () => {
       .end(done);
   });
 });
+
+describe("GET /todos", () => {
+  it("Should return an array of todo's objects", done => {
+    request(app)
+      .get("/todos")
+      .expect(200)
+      .expect(res => {
+        expect(Array.isArray(res.body.todos)).toBe(true);
+      })
+      .end(done);
+  });
+
+  it("A object received should have a text property", done => {
+    request(app)
+      .get("/todos")
+      .expect(200)
+      .expect(res => {
+        const item = res.body.todos[0];
+        expect(item).toHaveProperty("text");
+      })
+      .end(done);
+  });
+});
+
+describe("GET /todos/:id", () => {
+  const _id = "5c6ee921c1e0a1336c140365";
+  const old_id = "5c72c2e841d64e29bc70fa77";
+  const bad_id = "743";
+
+  it("Should return an error if the id is not valid", done => {
+    request(app)
+      .get(`/todos/${bad_id}`)
+      .expect(401)
+      .end(done);
+  });
+  
+  it("Should return an todo object with the same id", done => {
+    request(app)
+      .get(`/todos/${_id}`)
+      .expect(200)
+      .expect(res => {
+        const todo_ret = res.body;
+        expect(todo_ret._id).toBe(_id);
+      })
+      .end(done);
+  });
+
+  it("Should return an error 404; id valid but is not any more in the db", done => {
+    request(app)
+      .get(`/todos/${old_id}`)
+      .expect(404).end(done);
+  });
+});
